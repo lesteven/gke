841d4d612e3814b3ced9c979beeef6f28b6f9ee8
lesteven <le_steven@outlook.com>
2019-07-01 09:54:07 -0700

    WIP: add gke skeleton

---
+++ cluster/main.tf
@@ -0,0 +1,40 @@
+
+provider "google" {
+  credentials = "${var.json}"
+  project = "${var.project}"
+  region = "us-west2"
+  zone = "us-west2-a"
+}
+
+resource "google_container_cluster" "primary" {
+  name = "gkecluster"
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "gkepool" {
+  name = "gkepool"
+  cluster = "${google_container_cluster.primary.name}"
+  node_count = 1
+
+  node_config {
+    preemptible = true
+    machine_type = "n1-standard-1"
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
