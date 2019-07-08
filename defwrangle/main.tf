7d6cb47def5b906f7f2c251a3ea58be4a643b2b6
lesteven <le_steven@outlook.com>
2019-07-08 00:59:42 -0700

    feat: add remote-exex postgres

--- defwrangle/main.tf
+++ defwrangle/main.tf
@@ -5,3 +5,28 @@ provider "google" {
   region = "us-west2"
 }
 
+resource "google_compute_firewall" "db" {
+  name = "db"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports = ["5432"]
+  }
+  source_tags = ["postgres"]
+}
+
+resource "google_compute_router" "router" {
+  name = "router"
+  network = "default"
+  region = "us-west2"
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name = "nat"
+  router = "${google_compute_router.router.name}"
+  nat_ip_allocate_option = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
--- defwrangle/variables.tf
+++ defwrangle/variables.tf
@@ -1,2 +1,3 @@
 variable "json" {}
 variable "project" {}
+variable "user" {}
